350cfa9c8f4b7c8f660f915e7e22be86aa64f337
secondstar9729 <secondstar9729@gmail.com>
2024-08-18 14:17:31 +0800

    error-handle-0003

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,6 +7,10 @@ const oauth = new DiscordOauth2();
 router.post('/login', async (req, res) => {
   try {
     const code = req.body.code;
+     // Validate 'code' is present
+  if (!code) {
+    return res.status(400).json({ error: "Authorization code not provided" });
+  }
     const accessToken = await oauth.tokenRequest({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
@@ -14,7 +18,10 @@ router.post('/login', async (req, res) => {
       scope: "identify guilds",
       grantType: "authorization_code",
       redirectUri: process.env.REDIRECT_URI,
-    });
+    }).catch(err => {
+      console.error('Token request error:', err);
+      res.status(400).json({ error: err.message });
+    });;
 
     const user = await oauth.getUser(accessToken.access_token);
     const guilds = await oauth.getUserGuilds(accessToken.access_token);
@@ -37,14 +44,8 @@ router.post('/login', async (req, res) => {
       res.status(403).json({ error: "User is not a member of the server" });
     }
   } catch (error) {
-    res.status(500).json({ error: "Authentication failed",req_data:{
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      code: req.body,
-      scope: "identify guilds",
-      grantType: "authorization_code",
-      redirectUri: process.env.REDIRECT_URI,
-    } });
+    console.error(error);
+    res.status(500).json({ error: "Authentication failed" });
   }
 });
 
