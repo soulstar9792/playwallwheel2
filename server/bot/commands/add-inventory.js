6da56fe9a606d4be81ed391d21fead653e16eb0c
secondstar9729 <secondstar9729@gmail.com>
2024-08-29 23:44:50 +0800

    bot: add-inventory command permission edited

--- server/bot/commands/add-inventory.js
+++ server/bot/commands/add-inventory.js
@@ -4,26 +4,28 @@ const User = require('../../models/User');
 
 module.exports = {
     data: new SlashCommandBuilder()
-      .setName('add-inventory')
-      .setDescription('Add items to a user\'s inventory')
-      .addUserOption(option => 
-          option.setName('user')
+        .setName('add-inventory')
+        .setDescription('Add items to a user\'s inventory')
+        .addUserOption(option => 
+            option.setName('user')
                 .setDescription('The user to add items to')
                 .setRequired(true))
-      .addStringOption(option => 
-          option.setName('item')
+        .addStringOption(option => 
+            option.setName('item')
                 .setDescription('The type of item to add (common, uncommon, rare, legendary, mythic)')
                 .setRequired(true))
-      .addIntegerOption(option => 
-          option.setName('amount')
+        .addIntegerOption(option => 
+            option.setName('amount')
                 .setDescription('The amount of items to add')
                 .setRequired(true)),
-    
+
     async execute(interaction) {
-        // Check if the command is executed by the server owner
         const { user, guild } = interaction;
-        if (user.id !== guild.ownerId) {
-            return interaction.reply({ content: 'Only the server owner can use this command!', ephemeral: true });
+
+        // Check if the user has Administrator permission
+        const member = await guild.members.fetch(user.id);
+        if (!member.permissions.has('ADMINISTRATOR')) {
+            return interaction.reply({ content: 'You must have the Administrator permission to use this command!', ephemeral: true });
         }
 
         const targetUser = interaction.options.getUser('user');
@@ -41,12 +43,11 @@ module.exports = {
             return interaction.reply({ content: `Invalid item type. Valid types are: ${validItems.join(', ')}`, ephemeral: true });
         }
 
-        
-            // Find or create the user in the database
-            let userRecord = await User.findOne({ id: interaction.user.id });
-            if (!userRecord) {
-                userRecord = new User(interaction.user);
-            }
+        // Find or create the user in the database
+        let userRecord = await User.findOne({ id: targetUser.id });
+        if (!userRecord) {
+            userRecord = new User({ id: targetUser.id, inventory: { commonKeys: 0, uncommonKeys: 0, rareKeys: 0, legendaryKeys: 0, mythicKeys: 0 } });
+        }
 
         // Add items to the user's inventory based on the item type
         userRecord.inventory[`${item}Keys`] += amount; // adds a certain amount of the item type
