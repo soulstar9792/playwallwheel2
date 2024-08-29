f021acd69ac3e87e3f795e0c14889799fd57379b
secondstar9729 <secondstar9729@gmail.com>
2024-08-29 23:52:53 +0800

    bot: message create bug fix.

--- server/bot/events/messageCreate.js
+++ server/bot/events/messageCreate.js
@@ -9,17 +9,27 @@ module.exports = {
     // Find or create the user in the database
     let userRecord = await User.findOne({ id: message.author.id});
     if (!userRecord) {
-        userRecord = new User(message.author);
+        userRecord = new User({
+            id: message.author.id,
+            xp: 0,
+            level: 1,
+            messageCount: 0,
+            lastMessage: new Date()
+        });
     }
-    // user.inventory.communityCoinss += 1; // Reward for each message
+    
+    // user.inventory.communityCoins += 1; // Reward for each message
     userRecord.xp += 15;
+    
     if (userRecord.xp >= userRecord.level * 100) {
         userRecord.level += 1;
         userRecord.xp = 0;
-        message.reply(`Congratulations <@${userRecordId}>, you've leveled up to level ${userRecord.level}!`);
+        message.reply(`Congratulations <@${message.author.id}>, you've leveled up to level ${userRecord.level}!`);
     }
+    
     userRecord.messageCount += 1;
     userRecord.lastMessage = new Date();
+    
     await userRecord.save();
   },
 };
